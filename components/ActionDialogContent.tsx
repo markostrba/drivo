5984f2a1642e4d91b65c0263877fcbaeb1cf255d
mearko <76460780+markostrba@users.noreply.github.com>
2025-07-10 07:10:24 +0200

    Add FileDetails component

--- components/ActionDialogContent.tsx
+++ components/ActionDialogContent.tsx
@@ -10,6 +10,9 @@ import { Button } from "./ui/button";
 import { Models } from "appwrite";
 import { ActionType } from "@/types/global";
 import Image from "next/image";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
+import { convertFileSize, formatDateTime } from "@/lib/utils";
 
 type File = Models.Document;
 type OnNameChange = (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -22,6 +25,23 @@ interface Props {
   isLoading: boolean;
   onAction: () => void;
 }
+
+const ImageThumbnail = ({ file }: { file: File }) => (
+  <div className="border-text-light-2 text-light-1 flex h-[80px] items-center gap-[15px] rounded-[12px] border pl-3.5">
+    <Thumbnail type={file.type} extension={file.extension} url={file.url} />
+    <div className="flex flex-col">
+      <p className="subtitle-2 mb-1">{file.name}</p>
+      <FormattedDateTime date={file.$createdAt} className="caption" />
+    </div>
+  </div>
+);
+
+const DetailRow = ({ label, value }: { label: string; value: string }) => (
+  <div className="text-light-1 flex">
+    <p className="body-2 w-[30%] text-left opacity-60">{label}</p>
+    <p className="subtitle-2 flex-1 text-left">{value}</p>
+  </div>
+);
 
 const FileRename = ({
   onNameChange,
@@ -42,9 +62,21 @@ const FileRename = ({
 
 const FileDelete = (file: File) => {};
 
-const FileDetails = (file: File) => {};
+const FileDetails = ({ file }: { file: File }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <ImageThumbnail file={file} />
+      <div className="space-y-4.5 pl-2">
+        <DetailRow label="Format:" value={file.extension} />
+        <DetailRow label="Size:" value={convertFileSize(file.size)} />
+        <DetailRow label="Owner:" value={file.owner.fullName} />
+        <DetailRow label="Last edit:" value={formatDateTime(file.$updatedAt)} />
+      </div>
+    </div>
+  );
+};
 
-const FilShare = (file: File) => {};
+const FileShare = (file: File) => {};
 
 export const ActionDialogContent = ({
   action,
@@ -68,7 +100,7 @@ export const ActionDialogContent = ({
         {value === "rename" && (
           <FileRename onNameChange={onNameChange} value={name} />
         )}
-        {value === "details" && <div>file details</div>}
+        {value === "details" && <FileDetails file={file} />}
         {value === "share" && <div>share</div>}
         {value === "delete" && (
           <p className="text-light-1 text-center">

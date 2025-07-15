5a6f7f806c9aa914e34d9589342c56c7207b802a
markostrba <mark3strba@gmail.com>
2025-07-15 14:06:13 +0200

    Add toast component

---
+++ components/ErrorToast.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+import { toast } from "sonner";
+
+const ErrorToast = ({
+  error,
+  title,
+}: {
+  error?: { message: string };
+  title: string;
+}) => {
+  useEffect(() => {
+    if (error) {
+      toast.error(title, {
+        description: error.message,
+      });
+    }
+  }, [error]);
+
+  return null;
+};
+
+export default ErrorToast;
